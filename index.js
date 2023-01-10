9bfd9b024961f4358c75bf8d49482c0f3d3518c3
Anson Guo <anson.guo.921104@gmail.com>
2023-01-10 18:32:31 -0500

    commit tree c4698ed9add909f52ebf0baca8b7eebd6fae5b46

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const e1 = document.getElementById("main")
+e1.remove()
+
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Anson is the champion"
+
